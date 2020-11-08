990c4eaa2f4a557c5169493de5fdf805fb37509d
Ray Berger <RayBB@users.noreply.github.com>
2020-11-07 14:17:12 -1000

    cleanup mock

--- archive-site-frontend/src/app/mock-data/mock-documents.ts
+++ archive-site-frontend/src/app/mock-data/mock-documents.ts
@@ -2,27 +2,6 @@
 //import User from '../models/user';
 import { Document } from '../models/document';
 
-export const DOCUMENTS: Document[] = []
-//     {Id: 21, ProjectId: 1, DocumentImageUrl: "/images/some0.png"},
-//     {Id: 22, Project: 1, Path: "/images/some1.jpg"},
-//     {Id: 23, Project: 1, Path: "/images/some2.jpg"},
-//     {Id: 24, Project: 1, Path: "/images/some3.jpg"},
-//     {Id: 25, Project: 1, Path: "/images/some4.jpg"},
-//     {Id: 26, Project: 1, Path: "/images/some5.jpg"},
-//     {Id: 27, Project: 2, Path: "/images/some6.jpg"},
-//     {Id: 28, Project: 2, Path: "/images/some7.jpg"},
-//     {Id: 29, Project: 2, Path: "/images/some8.jpg"},
-//     {Id: 30, Project: 2, Path: "/images/some9.jpg"},
-//     {Id: 31, Project: 2, Path: "/images/some10.jpg"},
-//     {Id: 21, Project: 2, Path: "/images/some11.png"},
-//     {Id: 22, Project: 2, Path: "/images/some12.jpg"},
-//     {Id: 23, Project: 2, Path: "/images/some13.jpg"},
-//     {Id: 24, Project: 3, Path: "/images/some14.jpg"},
-//     {Id: 25, Project: 3, Path: "/images/some15.jpg"},
-//     {Id: 26, Project: 3, Path: "/images/some16.jpg"},
-//     {Id: 27, Project: 3, Path: "/images/some17.jpg"},
-//     {Id: 28, Project: 3, Path: "/images/some18.jpg"},
-//     {Id: 29, Project: 3, Path: "/images/some19.jpg"},
-//     {Id: 30, Project: 3, Path: "/images/some20.jpg"},
-//     {Id: 31, Project: 3, Path: "/images/some21.jpg"}
-// ];
+export const DOCUMENTS: Document[] = [
+    {Id: 21, ProjectId: 1, DocumentImageUrl: "/images/some0.png", FileName: "a.jpeg", Status: "Done"},
+];

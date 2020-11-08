4e1c05280231297b4f771de801e7d3babd0e5949
Ray Berger <RayBB@users.noreply.github.com>
2020-11-08 10:26:37 -1000

    add urls when creating new projects

--- archive-site-frontend/src/app/models/document.ts
+++ archive-site-frontend/src/app/models/document.ts
@@ -1,7 +1,12 @@
-export interface Document {
-    Id: number,
-    ProjectId: number,
-    FileName: string,
-    DocumentImageUrl: string,
-    Status: string,
+import Model from './model'
+
+export class Document extends Model {
+    constructor(
+        id: number = 0,
+        public ProjectId: number = undefined,
+        public FileName: string = undefined,
+        public DocumentImageUrl: string = undefined,
+        public Status: string = "PendingTranscription") {
+        super(id);
+      }
 }

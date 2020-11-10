e50a3a1400721504f2c3d4da406b80e08eeb4a56
Paul Wheeler <paul@invioinc.com>
2020-11-10 11:22:31 -1000

    Fixed manual field list in project creation. Project is now added to the inactive project list after creation.

--- archive-site-frontend/src/app/projects/projects.component.ts
+++ archive-site-frontend/src/app/projects/projects.component.ts
@@ -65,7 +65,7 @@ export class ProjectsComponent implements OnInit {
         .get()
         .pipe(map((oe: ODataEntities<Project>) => oe.entities));
 
-    this.user$
+    this._userContext.user$
       .subscribe(result => {
         if (result) {
           this.inactiveProjects$ =

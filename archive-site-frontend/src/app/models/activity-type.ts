f25d14b6fff77d16737774ec6824fe41b7101369
Paul Wheeler <paul@invioinc.com>
2020-11-15 13:03:37 -1000

    Implemented the activity feed for reals.

---
+++ archive-site-frontend/src/app/models/activity-type.ts
@@ -0,0 +1,7 @@
+export enum ActivityType {
+  UserSignup = 'UserSignup',
+  TranscriptionSubmitted = 'TranscriptionSubmitted',
+  ProjectCreated = 'ProjectCreated',
+  ProjectCompleted = 'ProjectCompleted',
+  TranscriptionApproved = 'TranscriptionApproved'
+}
--- archive-site-frontend/src/app/models/transcription.ts
+++ archive-site-frontend/src/app/models/transcription.ts
@@ -1,4 +1,6 @@
 import { Model } from './model';
+import { Document } from 'src/app/models/document';
+import { User } from 'src/app/models/user';
 
 export class Transcription extends Model {
   constructor(
@@ -7,7 +9,9 @@ export class Transcription extends Model {
     public UserId: number = undefined,
     public Data: string = undefined,
     public ValidationErrors: string = undefined,
-    public IsSubmitted: boolean = false) {
+    public IsSubmitted: boolean = false,
+    public Document?: Document,
+    public User?: User) {
     super(id);
   }
 }

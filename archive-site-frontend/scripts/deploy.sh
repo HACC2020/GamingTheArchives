798bac3c1ae78e32ccccb70f23b020b37af12f6a
Paul Wheeler <paul@invioinc.com>
2020-11-20 00:15:12 -1000

    Update deployment script to override index.html Cache-Control setting.

--- archive-site-frontend/scripts/deploy.sh
+++ archive-site-frontend/scripts/deploy.sh
@@ -4,3 +4,4 @@ set -e
 
 ng build --prod
 gsutil rsync -r ./dist/archive-site-frontend gs://app-hiscribe-frontend/
+gsutil setmeta  -h "Cache-control:no-store, max-age=0" gs://app-hiscribe-frontend/index.html
